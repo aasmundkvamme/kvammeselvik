322a0ad85d7bf44623d96ca1a3e6e99cc8e960ca
Aasmund Kvamme <aasmund.kvamme@hvl.no>
2026-05-11 10:13:55 +0200

    Oppdatering frå jobb-PC

--- pelicanconf.py
+++ pelicanconf.py
@@ -9,7 +9,8 @@ DEFAULT_DATE_FORMAT = '%Y-%m-%d'
 # LOCALE = 'C.utf8'
 # DEFAULT_LANG = 'en'
 
-THEME = '../../pelican-themes/gum'
+# THEME = '../../pelican-themes/gum'
+THEME = 'notmyidea-cms'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
